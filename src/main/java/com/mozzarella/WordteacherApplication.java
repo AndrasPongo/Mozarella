8e45ee2c3f89b8ecb741a9fee244c6d503d5e93c
Pitypalatty <pitypalattysama@gmail.com>
2016-03-18 21:07:39 +0100

    initial commit

---
+++ src/main/java/com/mozzarella/WordteacherApplication.java
@@ -0,0 +1,12 @@
+package com.mozzarella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordteacherApplication.class, args);
+	}
+}
