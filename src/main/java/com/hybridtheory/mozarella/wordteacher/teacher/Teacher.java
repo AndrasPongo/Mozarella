17fb3deed49297889a23571aac124d3a9e8fba3e
Pitypalatty <pitypalattysama@gmail.com>
2016-03-20 19:43:31 +0100

    fix test testEvaluations_answerIsNotCorrect

--- src/main/java/com/hybridtheory/mozarella/wordteacher/teacher/Teacher.java
+++ src/main/java/com/hybridtheory/mozarella/wordteacher/teacher/Teacher.java
@@ -5,10 +5,6 @@ import com.hybridtheory.mozarella.wordteacher.learnmaterials.Word;
 public class Teacher {
 	
 	public boolean Evaluate(Word word, String answer) {
-		if (word.getTranslation() == answer) {
-			return true;
-		} else {
-			return false;
-		}
-	}	
+		return word.getTranslation().equals(answer);	
+	}
 }
