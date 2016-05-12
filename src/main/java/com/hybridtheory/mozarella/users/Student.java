fddaafba69f2461d86c97719b9bc62bea4213c20
AndrasPongo <andras.pongo14@gmail.com>
2016-05-12 20:17:23 +0200

    Initial security config - not working with real user yet
    Username: user
    Password: in the logs at "Using default security password"

--- src/main/java/com/hybridtheory/mozarella/users/Student.java
+++ src/main/java/com/hybridtheory/mozarella/users/Student.java
@@ -1,5 +1,7 @@
 package com.hybridtheory.mozarella.users;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -12,6 +14,10 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hybridtheory.mozarella.pet.Pet;
 import com.hybridtheory.mozarella.pet.cubefish.CubeFish;
@@ -21,7 +27,7 @@ import com.hybridtheory.mozarella.wordteacher.learnmaterials.LearnItemList;
 import com.hybridtheory.mozarella.wordteacher.learnmaterials.LearnItemsManager;
 
 @Entity
-public class Student {
+public class Student implements UserDetails {
 	
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
@@ -33,6 +39,7 @@ public class Student {
 	private Integer googleId;
 	
 	private String name = "";
+	private String password = "";
 	
 	@JsonIgnore
 	@OneToOne(cascade = {CascadeType.ALL}, fetch=FetchType.LAZY, optional = false)
@@ -54,6 +61,10 @@ public class Student {
 		return this.name;
 	}
 	
+	public String getPassword() {
+		return this.password;
+	}
+	
 	public void initialize(String studentName) {
 		boolean validName = false;
 		validName = inputSanitizer.checkStudentNameIsValid(studentName);
@@ -61,6 +72,7 @@ public class Student {
 			throw new IllegalArgumentException("Invalid name for Student");
 		}
 		this.name = studentName;
+		this.password = "password";
 		learnItemManager = new LearnItemsManager(this);
 	}
 	
@@ -129,6 +141,40 @@ public class Student {
 
 	public Integer getId() {
 		return id;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+	    return Arrays.asList(new SimpleGrantedAuthority("ROLE_STUDENT"));
+	}
+
+	@Override
+	public String getUsername() {
+		return this.name;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
